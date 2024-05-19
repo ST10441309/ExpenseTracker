fb6d4920ea35469d722f2850d1710a3d8aca379f
ST10441309 <ST10441309@vcconnect.edu.za>
2024-05-19 21:51:56 +0200

    first screen logic

--- app/src/main/java/com/st10441309/expensetracker/MainActivity.kt
+++ app/src/main/java/com/st10441309/expensetracker/MainActivity.kt
@@ -2,10 +2,32 @@ package com.st10441309.expensetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+    private var tvDisplayAmount: TextView? = null
+    private var tvDisplayIncome: TextView? = null
+    private var tvDisplayExpense: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvDisplayAmount = findViewById(R.id.tvDisplayAmount)
+        tvDisplayIncome = findViewById(R.id.tvDisplayIncome)
+        tvDisplayExpense = findViewById(R.id.tvDisplayExpense)
+
+        val btnIncomeSheet = findViewById<Button>(R.id.btnIncome)
+        btnIncomeSheet.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+        val btnExpenseSheet = findViewById<Button>(R.id.btnExpense)
+        btnExpenseSheet.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/st10441309/expensetracker/MainActivity3.kt
@@ -0,0 +1,5 @@
+package com.st10441309.expensetracker
+
+class MainActivity3 {
+
+}
