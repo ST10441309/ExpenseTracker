82f5b8787900c3070ffcebb8f649f759e2389c84
ST10441309 <ST10441309@vcconnect.edu.za>
2024-05-25 22:51:46 +0200

    screens linking

--- app/src/main/java/com/st10441309/expensetracker/MainActivity2.kt
+++ app/src/main/java/com/st10441309/expensetracker/MainActivity2.kt
@@ -1,14 +1,32 @@
 package com.st10441309.expensetracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
-import android.content.Intent
+import android.widget.Button
+
 class MainActivity2 : AppCompatActivity() {
+    private var tvIncomeDescription: TextView? = null
+    private var tvIncomeTotal: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        tvIncomeDescription = findViewById(R.id.tvIncomeDescription)
+        tvIncomeTotal = findViewById(R.id.tvIncomeTotal)
+
+        val btnExpenseSheet = findViewById<Button>(R.id.btnExpense2)
+        btnExpenseSheet.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
+
+        val btnMainScreen = findViewById<Button>(R.id.btnMain)
+        btnMainScreen.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/st10441309/expensetracker/MainActivity3.kt
+++ app/src/main/java/com/st10441309/expensetracker/MainActivity3.kt
@@ -7,8 +7,25 @@ import android.widget.TextView
 import android.content.Intent
 
 class MainActivity3 : AppCompatActivity() {
+    private var tvExpenseDescription: TextView? = null
+    private var tvExpenseTotal: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        tvExpenseDescription = findViewById(R.id.tvExpenseDescription)
+        tvExpenseTotal = findViewById(R.id.tvExpenseTotal)
+
+        val btnIncomeSheet = findViewById<Button>(R.id.btnIncome2)
+        btnIncomeSheet.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        val btnMainScreen = findViewById<Button>(R.id.btnMain2)
+        btnMainScreen.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
